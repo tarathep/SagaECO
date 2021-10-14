8add3d28daaac1f3f8d08e1112b548cf5293e3f6
tarathep <bokie.tarathep@gmail.com>
2021-10-14 22:09:19 +0700

    script to docker update

---
+++ deploy.bat
@@ -0,0 +1,5 @@
+docker stop sagamap sagalogin
+docker rm sagamap sagalogin
+docker rmi kietara/sagamap:latest kietara/sagalogin:latest
+docker run -d --restart always --name sagalogin -p 12200:12200 -v d:/SagaConfig/Config:/Config -v d:/SagaConfig/Log:/Log kietara/sagalogin:latest
+docker run -d --restart always --name sagamap -p 12201:12201 -v d:/SagaConfig/Config:/Config -v d:/SagaConfig/Log:/Log kietara/sagamap:latest
